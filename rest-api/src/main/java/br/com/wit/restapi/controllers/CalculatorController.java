7fe825e22f6f3a5d6f5ec8e8f254173a0a8b1f06
Marcos Diniz <marcos.diniz@sicoob.com.br>
2022-04-12 09:56:55 -0300

    configuraçao documentacao swagger, escopo dos endpoints e service

---
+++ rest-api/src/main/java/br/com/wit/restapi/controllers/CalculatorController.java
@@ -0,0 +1,69 @@
+package br.com.wit.restapi.controllers;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.wit.restapi.services.CalculatorService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@RestController
+@RequestMapping(value = "/calculator")
+public class CalculatorController {
+	
+	@Autowired
+	private CalculatorService calculatorService;
+	
+	@ApiOperation(value = "Operation of sum")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Result of the sum"),
+			@ApiResponse(code = 404, message = "Operation invalid") })
+	@GetMapping(path = "sum", produces = "application/json", consumes = "application/json")
+	public ResponseEntity<BigDecimal> sum(@RequestParam BigDecimal a, @RequestParam BigDecimal b)  {
+		
+		BigDecimal result = calculatorService.sum(a, b, "");
+		
+		return ResponseEntity.ok(result);
+	}
+	
+	@ApiOperation(value = "Operation of Subtraction")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Subtraction result"),
+			@ApiResponse(code = 404, message = "Operation invalid") })
+	@GetMapping(path = "subtract", produces = "application/json", consumes = "application/json")
+	public ResponseEntity<BigDecimal> subtract(@RequestParam BigDecimal a, @RequestParam BigDecimal b)  {
+		
+		BigDecimal result = calculatorService.subtract(a, b, "");
+		
+		return ResponseEntity.ok(result);
+	}
+	
+	@ApiOperation(value = "Operation of multiplication")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Result of multiplication"),
+			@ApiResponse(code = 404, message = "Operation invalid") })
+	@GetMapping(path = "multiply", produces = "application/json", consumes = "application/json")
+	public ResponseEntity<BigDecimal> multiply(@RequestParam BigDecimal a, @RequestParam BigDecimal b)  {
+		
+		BigDecimal result = calculatorService.multiply(a, b, "");
+		
+		return ResponseEntity.ok(result);
+	}
+	
+	@ApiOperation(value = "Operation of division")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Result of division"),
+			@ApiResponse(code = 404, message = "Operation invalid") })
+	@GetMapping(path = "divide", produces = "application/json", consumes = "application/json")
+	public ResponseEntity<BigDecimal> divide(@RequestParam BigDecimal a, @RequestParam BigDecimal b)  {
+
+		BigDecimal result = calculatorService.divide(a, b, "");
+		
+		return ResponseEntity.ok(result);
+	}
+
+
+}
---
+++ rest-api/src/main/java/br/com/wit/restapi/services/CalculatorService.java
@@ -0,0 +1,33 @@
+package br.com.wit.restapi.services;
+
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+
+	public BigDecimal sum(BigDecimal a, BigDecimal b, String token) {
+		
+		
+		return null;
+	}
+
+	public BigDecimal subtract(BigDecimal a, BigDecimal b, String token) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public BigDecimal multiply(BigDecimal a, BigDecimal b, String token) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public BigDecimal divide(BigDecimal a, BigDecimal b, String token) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
